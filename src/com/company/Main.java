c1229fa1825324e3145dea21f73258feea1d5c4b
Cle-Vin <dai.clement.jeux@gmail.com>
2018-07-11 15:38:53 +0200

    Add mod and Scanner method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner lc = new Scanner(System.in);
+        System.out.println("Veuillez choisir votre humeur : ");
+        System.out.println("0 - Très Heureux/se");
+        System.out.println("1 - Heureux/se");
+        System.out.println("2 - Ok");
+        System.out.println("3 - Triste");
+        System.out.println("4 - Très Triste");
+        int humeur = lc.nextInt();
+        String[] humeurPossible = {"Très Heureux/se","Heureux/se","OK","Triste","Très Triste"};
+        System.out.println("Vous avez choisi comme humeur : " + humeurPossible[humeur]);
+
     }
 }
-\** ceci est un test
-\* j'ai fait une modif pour essayer
+/** ceci est un test
+/* j'ai fait une modif pour essayer
+ */
