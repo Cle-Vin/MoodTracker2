ad35aa50a3e5c2bda86b638fc4f05e7f23bf25ee
Cle-Vin <dai.clement.jeux@gmail.com>
2018-07-11 18:35:45 +0200

    Add method addMood, asking add mood or not. Correct string added to resume. No more error.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         Order order = new Order();
-        order.whichDay();
-        int i = 0
-        /** la methode "newScan" n'existe pas sur java
-         */
-        if newScan(){
-            order.addMood();
+        order.init();
         }
     }
-}
 /** ceci est un test
 /* j'ai fait une modif pour essayer
  */
--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -8,15 +8,15 @@ public class Order {
 
     public void moodChoice() {
         System.out.println("Veuillez choisir votre humeur : ");
-        System.out.println("0 - Très Heureux/se");
-        System.out.println("1 - Heureux/se");
-        System.out.println("2 - Ok");
-        System.out.println("3 - Triste");
-        System.out.println("4 - Très Triste");
+        System.out.println("1 - Très Heureux/se");
+        System.out.println("2 - Heureux/se");
+        System.out.println("3 - Ok");
+        System.out.println("4 - Triste");
+        System.out.println("5 - Très Triste");
         int humeur = lc.nextInt();
-        String[] humeurPossible = {"Très Heureux/se", "Heureux/se", "OK", "Triste", "Très Triste"};
-        System.out.println("Vous avez choisi comme humeur : " + humeurPossible[humeur]);
-        resume += ": " + humeurPossible[humeur] + " ";
+        String[] humeurPossible = {"Très Heureux/se", "Heureux/se", "Ok", "Triste", "Très Triste"};
+        System.out.println("Vous avez choisi comme humeur : " + humeurPossible[(humeur-1)]);
+        resume += humeurPossible[(humeur-1)];
     }
 
     public void whichDay() {
@@ -31,11 +31,36 @@ public class Order {
         int jour = lc.nextInt();
         String[] jourPossible = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
         System.out.println("Nous sommes le " + jourPossible[(jour - 1)]);
-        resume += jourPossible[(jour - 1)] + " ";
+        resume += jourPossible[(jour - 1)] + " : ";
     }
-    public void addMood(){
+
+    public void addMood() {
+        int humeurSupplementaire = 0;
+        do {
+            System.out.println("voulez-vous ajouter une autre humeur ?");
+            System.out.println("1 - Oui");
+            System.out.println("2 - Non");
+            humeurSupplementaire = lc.nextInt();
+            switch (humeurSupplementaire) {
+                case 1:
+                    resume += ", ";
+                    moodChoice();
+                    break;
+                case 2:
+                    System.out.println("Vous ne souhaitez pas ajouter d'humeur aujourd'hui.");
+                    break;
+                default:
+                    System.out.println("Vous n'avez pas choisi parmi les options proposées.");
+            }
+            System.out.println("");
+            System.out.println(String.format(resume));
+        }while (humeurSupplementaire != 2);
+    }
+
+
+    public void init(){
+        whichDay();
         moodChoice();
-        System.out.println("");
-        System.out.println(String.format(resume));
+        addMood();
     }
 }
